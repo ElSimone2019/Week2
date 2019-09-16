d4963e4e491b02a114a0fe25f9a7c817c3d15a3a
Ewan <646067@student.cavc.ac.uk>
2019-09-16 10:27:22 +0100

    Add project files.

---
+++ Week2.cpp
@@ -0,0 +1,14 @@
+// Week2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    //Variables to hold the user choices in
+	string playerOne = " ";
+	string playerTwo = " ";
+
+}	
