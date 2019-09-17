a022ef72c104651624e53710b8b75848bc587a46
Ewan <646067@student.cavc.ac.uk>
2019-09-17 11:40:25 +0100

    Update

--- Week2.cpp
+++ Week2.cpp
@@ -11,5 +11,22 @@ int main()
 	string playerOne = " ";
 	string playerTwo = " ";
 	
+	//Player One Choice
+	cout << "Please pick 'Rock', 'Paper', or 'Scissors'";
+	cin >> playerOne;
 
+	//Player Two Choice
+	cout << "Please pick 'Rock', 'Paper', or 'Scissors'";
+	cin >> playerTwo;
+
+	//Choice of Rock Paper Scissors
+	if (playerOne == "Rock" && playerTwo == "Rock") {
+		cout << "Draw";
+	}
+	else if (playerOne == "Paper" && playerTwo == "Paper") {
+		cout << "Draw";
+	}
+	else if (playerOne == "Scissors" && playerTwo == "Scissors") {
+		cout << "Draw";
+	}
 }	
