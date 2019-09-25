79bdc7fc0d1bae07c36f1c7cabb85d11358722bd
Ewan <646067@student.cavc.ac.uk>
2019-09-25 08:55:34 +0100

    Added vending activity

---
+++ Week2Vending.cpp
@@ -0,0 +1,28 @@
+// Week2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	//Variable to hold Selection, Snack Costs, The amount they will pay, and their change
+	string select;
+	float mars = 0.80;
+	float snickers = 0.90;
+	float bounty = 0.60;
+	float cash = 0.00;
+	float change = 0.00;
+
+	//Input for selection
+	cout << "Please input your choice of either mars, snickers or bounty\n: ";
+	cin >> select;
+
+	//Input for amount of cash that will be used to pay
+	cout << "Please input the amount of cash you will use\n: ";
+	cin >> cash;
+
+	//if statement to compare 
+
+}
